6a4335038d632e9f492a20b4b3b0321ce5794f25
Egor <aldes@list.ru>
2022-12-14 17:08:51 +0300

    1 task

---
+++ 1_task/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+Не использовать функцию Math.Pow
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16
+*/
+
+
+Console.WriteLine("введите А");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите B");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int result = 1;
+
+for (int i=0; i<B; i++)
+{
+    result = A * result;
+
+}
+Console.WriteLine(result);
